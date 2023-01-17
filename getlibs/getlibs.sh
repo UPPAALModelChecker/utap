ecb1568d8c4d1ab94619845a45475dd9f013173e
Marius Mikučionis <marius@cs.aau.dk>
2023-01-17 08:46:57 +0100

    Fixed doctest with precompiled setup (seems that doctest_with_main does not get installed)

--- getlibs/getlibs.sh
+++ getlibs/getlibs.sh
@@ -49,7 +49,7 @@ for target in "$@" ; do
         echo -e "${BW}${target}: Configuring ${DOCTEST}${NC}"
         cmake -S "$SOURCE/$DOCTEST" -B "$BUILD" -DCMAKE_TOOLCHAIN_FILE="$PROJECT_DIR/cmake/toolchain/${target}.cmake" \
           -DCMAKE_PREFIX_PATH="$LIBS" -DCMAKE_INSTALL_PREFIX="$LIBS" -DCMAKE_BUILD_TYPE=Release \
-          -DDOCTEST_WITH_TESTS=OFF
+          -DDOCTEST_WITH_TESTS=OFF -DDOCTEST_WITH_MAIN_IN_STATIC_LIB=ON
         echo -e "${BW}${target}: Building ${DOCTEST}${NC}"
         cmake --build "$BUILD"
         #echo -e "${BW}${target}: Testing ${DOCTEST}${NC}"
