7cf9e4b47d09ec41eb5bcd51a69cec2700d140d1
Marius Mikučionis <marius@cs.aau.dk>
2023-01-19 10:29:50 +0100

    Refactored document fixture and completed a unit test for unbound reference parameter

--- test/test_typechecker.cpp
+++ test/test_typechecker.cpp
@@ -28,9 +28,9 @@ TEST_SUITE("Quantifier sum")
 {
     TEST_CASE("sum expression")
     {
-        document_fixture f;
-        f.add_system_decl("int x = sum (index : int[0, 5]) index;");
-        auto document = f.parse();
+        auto df = document_fixture{};
+        df.add_system_decl("int x = sum (index : int[0, 5]) index;");
+        auto document = df.add_default_process().parse();
 
         CHECK(document->getErrors().size() == 0);
         CHECK(document->getWarnings().size() == 0);
@@ -38,10 +38,10 @@ TEST_SUITE("Quantifier sum")
 
     TEST_CASE("sum over array")
     {
-        document_fixture f;
-        f.add_system_decl("int a[3] = {1,4,9};");
-        f.add_system_decl("int x = sum(i : int[0, 2]) a[i];");
-        auto document = f.parse();
+        auto df = document_fixture{};
+        df.add_system_decl("int a[3] = {1,4,9};");
+        df.add_system_decl("int x = sum(i : int[0, 2]) a[i];");
+        auto document = df.add_default_process().parse();
         CHECK(document->getWarnings().size() == 0);
         const auto errors = document->getErrors();
         REQUIRE(errors.size() == 1);
@@ -50,10 +50,10 @@ TEST_SUITE("Quantifier sum")
 
     TEST_CASE("sum over const array")
     {
-        document_fixture f;
-        f.add_system_decl("const int a[3] = {1,4,9};");
-        f.add_system_decl("int x = sum(i : int[0, 2]) a[i];");
-        auto document = f.parse();
+        auto df = document_fixture{};
+        df.add_system_decl("const int a[3] = {1,4,9};");
+        df.add_system_decl("int x = sum(i : int[0, 2]) a[i];");
+        auto document = df.add_default_process().parse();
         CHECK(document->getWarnings().size() == 0);
         CHECK(document->getErrors().size() == 0);
     }
@@ -63,9 +63,9 @@ TEST_SUITE("Quantifier forall")
 {
     TEST_CASE("forall expression")
     {
-        document_fixture f;
-        f.add_system_decl("bool x = forall(index : int[0, 5]) index > 3;");
-        auto document = f.parse();
+        auto df = document_fixture{};
+        df.add_system_decl("bool x = forall(index : int[0, 5]) index > 3;");
+        auto document = df.add_default_process().parse();
 
         CHECK(document->getErrors().size() == 0);
         CHECK(document->getWarnings().size() == 0);
@@ -73,10 +73,10 @@ TEST_SUITE("Quantifier forall")
 
     TEST_CASE("forall over array")
     {
-        document_fixture f;
-        f.add_system_decl("bool b[3] = {1,1,1};");
-        f.add_system_decl("bool x = forall(i : int[0,2]) b[i];");
-        auto document = f.parse();
+        auto df = document_fixture{};
+        df.add_system_decl("bool b[3] = {1,1,1};");
+        df.add_system_decl("bool x = forall(i : int[0,2]) b[i];");
+        auto document = df.add_default_process().parse();
         CHECK(document->getWarnings().size() == 0);
         const auto errors = document->getErrors();
         REQUIRE(errors.size() == 1);
@@ -85,10 +85,10 @@ TEST_SUITE("Quantifier forall")
 
     TEST_CASE("forall over const array")
     {
-        document_fixture f;
-        f.add_system_decl("const bool b[3]={1,1,1};");
-        f.add_system_decl("bool x = forall(i : int[0,2]) b[i];");
-        auto document = f.parse();
+        auto df = document_fixture{};
+        df.add_system_decl("const bool b[3]={1,1,1};");
+        df.add_system_decl("bool x = forall(i : int[0,2]) b[i];");
+        auto document = df.add_default_process().parse();
         CHECK(document->getWarnings().size() == 0);
         CHECK(document->getErrors().size() == 0);
     }
@@ -98,18 +98,18 @@ TEST_SUITE("Quantifier exists")
 {
     TEST_CASE("exists expression")
     {
-        document_fixture f;
-        f.add_system_decl("bool x = exists(index : int[0, 5]) index > 3;");
-        auto document = f.parse();
+        auto df = document_fixture{};
+        df.add_system_decl("bool x = exists(index : int[0, 5]) index > 3;");
+        auto document = df.add_default_process().parse();
         CHECK(document->getErrors().size() == 0);
         CHECK(document->getWarnings().size() == 0);
     }
     TEST_CASE("exists over array")
     {
-        document_fixture f;
-        f.add_system_decl("bool b[3]={0,0,1};");
-        f.add_system_decl("bool x = exists(i : int[0,2]) b[i];");
-        auto document = f.parse();
+        auto df = document_fixture{};
+        df.add_system_decl("bool b[3] = {0,0,1};");
+        df.add_system_decl("bool x = exists(i : int[0,2]) b[i];");
+        auto document = df.add_default_process().parse();
         CHECK(document->getWarnings().size() == 0);
         const auto errors = document->getErrors();
         REQUIRE(errors.size() == 1);
@@ -117,11 +117,72 @@ TEST_SUITE("Quantifier exists")
     }
     TEST_CASE("exists over const array")
     {
-        document_fixture f;
-        f.add_system_decl("const bool b[3]={0,0,1};");
-        f.add_system_decl("bool x = exists(i : int[0,2]) b[i];");
-        auto document = f.parse();
+        auto df = document_fixture{};
+        df.add_system_decl("const bool b[3]={0,0,1};");
+        df.add_system_decl("bool x = exists(i : int[0,2]) b[i];");
+        auto document = df.add_default_process().parse();
         CHECK(document->getWarnings().size() == 0);
         CHECK(document->getErrors().size() == 0);
+    }
+}
+
+TEST_SUITE("Error positions for unbound parameters")
+{
+    TEST_CASE("Bounded const int parameter")
+    {
+        auto df = document_fixture{};
+        df.add_template(template_fixture{"T"}.add_parameter("const int[1,4] test").str());
+        df.add_process("T");
+        auto text = df.str();
+        auto doc = df.parse();
+        auto warns = doc->getWarnings();
+        CHECK(warns.size() == 0);
+        auto errs = doc->getErrors();
+        CHECK(errs.size() == 0);
+    }
+
+    TEST_CASE("Bounded int parameter")
+    {
+        auto df = document_fixture{};
+        df.add_template(template_fixture{"T"}.add_parameter("int[1,4] test").str());
+        df.add_process("T");
+        auto text = df.str();
+        auto doc = df.parse();
+        auto warns = doc->getWarnings();
+        CHECK(warns.size() == 0);
+        auto errs = doc->getErrors();
+        CHECK(errs.size() == 0);
+    }
+
+    TEST_CASE("Unbounded int parameter")
+    {
+        auto df = document_fixture{};
+        df.add_template(template_fixture{"T"}.add_parameter("int test").str());
+        df.add_process("T");
+        auto text = df.str();
+        auto doc = df.parse();
+        auto warns = doc->getWarnings();
+        CHECK(warns.size() == 0);
+        auto errs = doc->getErrors();
+        CHECK(errs.size() == 1);
+        auto pos = errs.front().position;
+        CHECK(pos.start != pos.unknown_pos);
+        CHECK(pos.end != pos.unknown_pos);
+    }
+
+    TEST_CASE("Reference parameter")
+    {
+        auto df = document_fixture{};
+        df.add_template(template_fixture{"T"}.add_parameter("int& test").str());
+        df.add_process("T");
+        auto text = df.str();
+        auto doc = df.parse();
+        auto warns = doc->getWarnings();
+        CHECK(warns.size() == 0);
+        auto errs = doc->getErrors();
+        CHECK(errs.size() == 1);
+        auto pos = errs.front().position;
+        CHECK(pos.start != pos.unknown_pos);
+        CHECK(pos.end != pos.unknown_pos);
     }
 }
