510a890618e7649c44eb3889c1ddfc255865d62c
Marius Mikučionis <marius@cs.aau.dk>
2023-09-21 16:05:28 +0200

    Switch to github mirror

--- cmake/libxml2.cmake
+++ cmake/libxml2.cmake
@@ -16,11 +16,11 @@ else(LibXml2_FOUND)
     seT(LIBXML2_WITH_THREADS OFF CACHE BOOL "LibXml2 multithread support")
     set(LIBXML2_WITH_TESTS OFF CACHE BOOL "LibXml2 Tests")
     FetchContent_Declare(LibXml2
-      URL https://download.gnome.org/sources/libxml2/2.11/libxml2-2.11.5.tar.xz
-      URL_HASH SHA256=3727b078c360ec69fa869de14bd6f75d7ee8d36987b071e6928d4720a28df3a6
+      GIT_REPOSITORY https://github.com/GNOME/libxml2.git # mirror
       #GIT_REPOSITORY https://gitlab.gnome.org/GNOME/libxml2 # often down
-      #GIT_REPOSITORY https://github.com/GNOME/libxml2.git # mirror
-      #GIT_TAG v2.11.5
+      GIT_TAG v2.11.5
+      #URL https://download.gnome.org/sources/libxml2/2.11/libxml2-2.11.5.tar.xz
+      #URL_HASH SHA256=3727b078c360ec69fa869de14bd6f75d7ee8d36987b071e6928d4720a28df3a6
       GIT_SHALLOW ON
       GIT_PROGRESS ON
       UPDATE_DISCONNECTED ON
