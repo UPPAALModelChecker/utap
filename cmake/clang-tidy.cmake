737e43d7cf37c4c99114e2144480779d1b797b14
Marius Mikučionis <marius@cs.aau.dk>
2024-04-12 11:07:31 +0200

    Fixed clang-tidy configuration

--- cmake/clang-tidy.cmake
+++ cmake/clang-tidy.cmake
@@ -1,4 +1,4 @@
-find_program(CLANG_TIDY_PROGRAM clang-tidy-14 clang-tidy)
+find_program(CLANG_TIDY_PROGRAM clang-tidy-16 clang-tidy-15 clang-tidy-14 clang-tidy)
 if(CLANG_TIDY_PROGRAM)
     execute_process(COMMAND ${CLANG_TIDY_PROGRAM} --version OUTPUT_VARIABLE CLANG_TIDY_VERSION_STRING)
     string(REGEX MATCH "LLVM version ([0-9A-Za-z]+)\\.([0-9A-Za-z\\.]+)" TIDY_VERSION "${CLANG_TIDY_VERSION_STRING}")
@@ -10,7 +10,7 @@ if(CLANG_TIDY_PROGRAM)
     if (TIDY_MAJOR_VERSION GREATER_EQUAL 14)
         # Add good checks which produce a minimal amount of false positives
         # One or two false positives can be dealt with using // NOLINT
-        set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_PROGRAM} -checks=-*,cppcoreguidelines-macro-usage,hicpp-deprecated-headers,modernize-deprecated-headers,hicpp-use-override,hicpp-use-emplace,modernize-use-emplace,hicpp-use-auto,readability-container-size-empty,readability-implicit-bool-conversion,readability-redundant-smartptr-get,readability-qualified-auto,performance-unnecessary-value-param,modernize-make-unique,modernize-make-shared,misc-unused-using-decls,performance-move-const-arg,modernize-use-using,modernize-use-nullptr,modernize-deprecated-headers,modernize-loop-convert,misc-unused-using-decls,misc-static-assert,misc-redundant-expression,modernize-use-bool-literals,readability-delete-null-pointer,readability-redundant-member-init --warnings-as-errors=-*)
+        set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_PROGRAM})
         message(STATUS "Enabled clang-tidy ${TIDY_VERSION}: ${CLANG_TIDY_PROGRAM}")
     else()
         message(WARNING "Found clang-tidy ${TIDY_VERSION}, but >=14 is required, thus disabled.")
