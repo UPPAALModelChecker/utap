3b572078e995e0c6051056459d5db749d6957d7e
Marius Mikučionis <marius@cs.aau.dk>
2025-10-16 18:14:24 +0200

    More debugging: added UTAP_STATIC and wine32

--- examples/compile-with-getlibs.sh
+++ examples/compile-with-getlibs.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 set -e
+
+function show_vars() {
+  echo "Environment variable values:"
+  for var in CMAKE_BUILD_TYPE CMAKE_TOOLCHAIN_FILE CMAKE_PREFIX_PATH CMAKE_INSTALL_PREFIX CMAKE_GENERATOR ; do
+    echo "    ${var}=${!var:-(unset)}"
+  done
+}
+
 # Absolute path to the example directory:
 EXAMPLE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Path to UTAP library source directory:
@@ -13,15 +21,18 @@ if [ -z "$TARGET" ] ; then
   TARGET="${ARCH}-${KERNEL}"
 fi
 
+# Toolchain to use:
+[ -n "$CMAKE_TOOLCHAIN_FILE" ] || export CMAKE_TOOLCHAIN_FILE="${UTAP_SRC}/cmake/toolchain/${TARGET}.cmake"
 # Where the dependent libraries are stored:
 [ -n "$CMAKE_PREFIX_PATH" ] || export CMAKE_PREFIX_PATH="${EXAMPLE_DIR}/local/${TARGET}"
 # Where to install the libraries:
 [ -n "$CMAKE_INSTALL_PREFIX" ] || export CMAKE_INSTALL_PREFIX="${EXAMPLE_DIR}/local/${TARGET}"
 # The build type:
 [ -n "$CMAKE_BUILD_TYPE" ] || export CMAKE_BUILD_TYPE=Release
+show_vars
 "${UTAP_SRC}/compile.sh" "$TARGET"
 
 BUILD_DIR="${EXAMPLE_DIR}/build-${TARGET}-${CMAKE_BUILD_TYPE,,}"
-cmake -B "$BUILD_DIR" -S "$EXAMPLE_DIR" -DCMAKE_TOOLCHAIN_FILE="${UTAP_SRC}/cmake/toolchain/${TARGET}.cmake"
+cmake -B "$BUILD_DIR" -S "$EXAMPLE_DIR"
 cmake --build "$BUILD_DIR"
 ctest --test-dir "$BUILD_DIR" --output-on-failure
